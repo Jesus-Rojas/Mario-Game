4ca5cbb48df1dff3cc07d4a4e20052afbe44cc17
Jesus Rojas <jarojas6524@misena.edu.co>
2022-04-09 18:12:25 -0500

    Move up player

--- main.js
+++ main.js
@@ -30,20 +30,15 @@ class Player {
   update() {
     this.draw()
     this.position.y += this.velocity.y
-
-    // if (this.position.y + this.height + this.velocity.y <= canvas.height) {
-    //   this.velocity.x += gravity
-    // } else {
-    //   this.velocity.y = 0
-    // }
-
-    this.velocity.y += gravity
+    if (this.position.y + this.height + this.velocity.y <= canvas.height) {
+      this.velocity.y += gravity
+    } else {
+      this.velocity.y = 0
+    }
   }
 }
 
-
 const player = new Player()
-
 
 function animate () {
   c.clearRect(0, 0, canvas.width, canvas.height)
