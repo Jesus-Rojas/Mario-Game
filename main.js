a9e7e29d27cbb9ce740ea87366be4aa9d6ce42ea
Jesus Rojas <jarojas6524@misena.edu.co>
2022-04-09 18:54:59 -0500

    Move (up | left | right) player

--- main.js
+++ main.js
@@ -30,6 +30,7 @@ class Player {
   update() {
     this.draw()
     this.position.y += this.velocity.y
+    this.position.x += this.velocity.x
     if (this.position.y + this.height + this.velocity.y <= canvas.height) {
       this.velocity.y += gravity
     } else {
@@ -39,17 +40,32 @@ class Player {
 }
 
 const player = new Player()
+const keys = {
+  right: {
+    pressed: false
+  },
+  left: {
+    pressed: false
+  },
+}
 
 function animate () {
   c.clearRect(0, 0, canvas.width, canvas.height)
   requestAnimationFrame(animate)
   player.update()
+
+  if (keys.right.pressed) {
+    player.velocity.x = 5
+  } else if (keys.left.pressed) {
+    player.velocity.x = -5
+  } else {
+    player.velocity.x = 0
+  }
 }
 // player.draw()
 animate()
 
 addEventListener('keydown', ({ keyCode }) => {
-  console.log(keyCode)
   switch (keyCode) {
     case 38:
       // up
@@ -62,10 +78,38 @@ addEventListener('keydown', ({ keyCode }) => {
 
     case 37:
       // left
-
+      keys.left.pressed = true
       break;
     case 39:
       // right
+      // player.velocity.x = 1
+      keys.right.pressed = true
+      break;
+
+    default:
+      break;
+  }
+})
+
+addEventListener('keyup', ({ keyCode }) => {
+  switch (keyCode) {
+    case 38:
+      // up
+      // player.velocity.y -= 20
+      break;
+
+    case 40:
+      // down
+      break;
+
+    case 37:
+      // left
+      keys.left.pressed = false
+      break;
+    case 39:
+      // right
+      // player.velocity.x = 0
+      keys.right.pressed = false
       break;
 
     default:
