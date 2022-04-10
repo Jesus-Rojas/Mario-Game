973cbb75ded5523dc84461461b326d7d73866b65
Jesus Rojas <jarojas6524@misena.edu.co>
2022-04-09 19:14:15 -0500

    Platform

--- main.js
+++ main.js
@@ -1,14 +1,10 @@
 const canvas = document.querySelector('canvas')
-
 const c = canvas.getContext('2d')
-
+const gravity = 0.5
 canvas.width = innerWidth
 canvas.height = innerHeight
 
-const gravity = 0.5
-
 class Player {
-
   constructor() {
     this.position = {
       x: 100,
@@ -39,7 +35,24 @@ class Player {
   }
 }
 
+class Platform {
+  constructor() {
+    this.position = {
+      x: 200,
+      y: 250
+    }
+    this.width = 200
+    this.height = 20
+  }
+
+  draw() {
+    c.fillStyle = 'aqua'
+    c.fillRect(this.position.x, this.position.y, this.width, this.height)
+  }
+}
+
 const player = new Player()
+const platform = new Platform()
 const keys = {
   right: {
     pressed: false
@@ -49,10 +62,11 @@ const keys = {
   },
 }
 
-function animate () {
+function animate() {
   c.clearRect(0, 0, canvas.width, canvas.height)
   requestAnimationFrame(animate)
   player.update()
+  platform.draw()
 
   if (keys.right.pressed) {
     player.velocity.x = 5
@@ -61,8 +75,17 @@ function animate () {
   } else {
     player.velocity.x = 0
   }
+
+  // platform collision detecting
+  if (
+      player.position.y + player.height <= platform.position.y &&
+      player.position.y + player.height + player.velocity.y >= platform.position.y &&
+      player.position.x + player.width >= platform.position.x && 
+      player.position.x  <= platform.position.x + platform.width
+    ) {
+    player.velocity.y = 0
+  }
 }
-// player.draw()
 animate()
 
 addEventListener('keydown', ({ keyCode }) => {
